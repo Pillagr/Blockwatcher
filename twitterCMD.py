e91ce56152b13c27a75a996551150cfe4cc14d2e
Pillagr <Pillagr6@gmail.com>
2019-07-29 02:33:31 -0400

    .

--- twitterCMD.py
+++ twitterCMD.py
@@ -52,14 +52,12 @@ class Specific:
         data1 = b.BTCStart().getUpdate()    #possibly merge data1 and data2 into a list, subscript in outputs
         data2 = None                        # FIX get TXCOunt from BTCMod etc. 
         text = f'New Block: {data1[0]}\nHash: {data1[2]}\nTime: {data1[1]}\nSource: Blockstream.info'
-        
-        
         reply1 = f'It contained {data2} TXs'
 
 
         
         #General().thread(text, reply1)
-        return data1, data2, text
+        return data1, data2, text, reply1
 
     def turn_BW_off(self):  # make this reply to the previous post and possibly retweet itself? 
         """
